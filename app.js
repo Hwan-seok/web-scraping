ec9fde06f5ed37bb6c6ce94e26edcd17cd2f78ce
Hwan-seok <tttkhs@naver.com>
2019-12-14 10:14:05 +0900

    - 각 url별 병렬 페이지 호출 및 상품명 조회 구현 완료

--- app.js
+++ app.js
@@ -1,72 +1,115 @@
-const puppeteer = require("puppeteer");
-const log = console.log;
-const targetHost = "https://shopping.naver.com/";
-const req = nodes => {};
-const doPuppeteer = async () => {
+const puppeteer = require('puppeteer');
+const targetHost = 'https://shopping.naver.com/';
+
+const collectTargetCategoryUrls = browser => {
+  return new Promise(async (resolve, reject) => {
     try {
-        const browser = await puppeteer.launch({
-            headless: true, // headless모드는 터미널에서만 실행할건지의 여부
-            // slowMo:1000, // 실행속도
-            defaultViewport: {
-                width: 1980,
-                height: 1080,
-                isMobile: false
-            }
-        });
-        const page = await browser.newPage();
-        page.on("console", msg => console.log("PAGE LOG:", msg.text()));
-        await page.goto(targetHost);
-        await page.waitFor(1000);
+      const page = await browser.newPage();
+      page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+      await page.goto(targetHost);
+      await page.waitFor(1000);
 
-        // while (true) {
-        const categoryPageUrlList = await page.$$eval(
-            "#home_category_area > div.co_category_menu > ul > li > a",
-            nodes =>
-                nodes.map(category => {
-                    category.click();
-                    return [
-                        ...document.querySelectorAll(
-                            "#home_category_area .co_col strong a"
-                        )
-                    ].map(col => col.href);
-                })
-        );
+      // while (true) {
+      const categoryPageUrlList = await page.$$eval(
+        '#home_category_area > div.co_category_menu > ul > li > a',
+        nodes =>
+          nodes.map(category => {
+            category.click();
+            return [
+              ...document.querySelectorAll(
+                '#home_category_area .co_col strong a',
+              ),
+            ].map(col => col.href);
+          }),
+      );
 
-        const flattenUrlList = categoryPageUrlList.flat();
-        //     targetURLs.push(partyListPerPage);
-        //     const next = await page.$("._next");
-        //     const isNextable = await next.evaluate(
-        //         node => !node.classList.contains("off")
-        //     );
-        //     log(isNextable);
-        //     if (isNextable) {
-        //         console.log("nexting");
-        //         await next.click();
-        //         await page.waitFor(100);
-        //         continue;
-        //     }
-        //     break;
-        // }
-        // log(targetURLs);
-        // const result = targetURLs.flat();
-        // console.log("result", result);
-        // await page. nodes => {
-        //     nodes.map(node => node.firstElementChild.href);
-        // });
-        browser.close();
+      resolve(categoryPageUrlList.flat());
     } catch (err) {
-        console.log(err);
+      reject(err);
     }
+  });
 };
-console.log("scrapping start!!");
-doPuppeteer();
-/*
-[...document.querySelectorAll("#home_category_area > div.co_category_menu > ul > li")].map(li=>({
-    className : li.className,
-    click:li.click
-}
-))
-*/
+
+const openPages = (browser, urls) => {
+  return new Promise(async (resolve, reject) => {
+    const promises = [];
+    const len = urls.length;
+    console.log(len);
+    for (let i = 0; i < len; i++) {
+      promises.push(
+        browser.newPage().then(async page => {
+          page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+          await page.goto(urls[i]);
+          return await page.$$eval(
+            '#_search_list > div.search_list.basis > ul > li .info .tit',
+            nodes => {
+              return nodes.map(product => {
+                return product.innerText;
+              });
+            },
+          );
+        }),
+      );
+    }
+    // const a = await Promise.all(promises);
+    console.log(a);
+    console.log(a.length);
+    // const page = await browser.newPage();
+    // await page.goto(urls[0]);
+    // const result = await page.$$eval(
+    //   '#_search_list > div.search_list.basis > ul > li .info .tit',
+    //   nodes => {
+    //     return nodes.map(product => {
+    //       return product.innerText;
+    //     });
+    //   },
+    // );
+
+    // for (let i = 0; i < 10; i++) {
+    //   promises.push(
+    //     browser.newPage().then(async page => {
+    //       await page.goto(urls[i]);
+    //       return await page.$$eval(
+    //         '#_search_list > div.search_list.basis > ul > li .info .tit',
+    //         nodes => {
+    //           console.log(nodes.length);
+    //           console.log(nodes);
+    //           return nodes.map(product => {
+    //             return product.innerText;
+    //           });
+    //         },
+    //       );
+    //     }),
+    //   );
+    // }
+
+    // await Promise.all(promises);
+    // browser.close();
+  });
+};
+console.log('scrapping start!!');
+
+const main = async () => {
+  const browser = await puppeteer.launch({
+    headless: false, // headless모드는 터미널에서만 실행할건지의 여부
+    // slowMo:1000, // 실행속도
+    defaultViewport: {
+      width: 1980,
+      height: 1080,
+      isMobile: false,
+    },
+  });
+  try {
+    const categoryUrls = await collectTargetCategoryUrls(browser);
+    console.log(categoryUrls);
+    await openPages(browser, categoryUrls);
+    browser.close();
+  } catch (err) {
+    console.log(err);
+  }
+};
+main();
+
 const collectEntryPoint = () => {
-    return Promise((resolve, reject) => {});
+  return Promise((resolve, reject) => {});
 };
